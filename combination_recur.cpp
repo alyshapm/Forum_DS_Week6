bd962e0f356d525abb96c24c5ad2c0ab2d24d4d7
alyshapm <91533472+alyshapm@users.noreply.github.com>
2022-03-27 19:24:18 +0700

    Init

--- combination_recur.cpp
+++ combination_recur.cpp
@@ -14,7 +14,9 @@ double combinationRecur(double n, double r) {
 int main(){
     double N, R; // declare var for n (number of item in set) and r (number of items selected from the set)
     int comb; // declare var to receive no. of possibler combinations from the function
-    
+    int comb2;
+    int comb3;
+
     // user input
     cout << "Enter the number of elements in the set (n): ";
     cin >> N;
@@ -23,6 +25,10 @@ int main(){
 
     // call function with n and r as parameter
     comb = combinationRecur(N, R);
+    comb2 = combinationRecur(5, 3);
+    comb3 = combinationRecur(13, 4);
 
-    cout<< "\nThe number of possible combinations (nCr) are = "<< comb <<endl; // print result
+    cout<< "\nThe number of possible combinations (nCr) are = "<< comb; // print result
+    cout<< "\nThe number of possible combinations when n=5 and r=3 are = "<< comb2;
+    cout<< "\nThe number of possible combinations when n=13 and r=4 are = "<< comb2 <<endl;
 }
